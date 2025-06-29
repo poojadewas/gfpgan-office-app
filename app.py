027d8a5f0e00b9f4298e5255ddf3ea18d9374909
poojadewas <135793847+poojadewas@users.noreply.github.com>
2025-06-29 20:33:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,4 +24,6 @@ def index():
     return render_template("index.html", output_url=output_url)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port, debug=True)
+
