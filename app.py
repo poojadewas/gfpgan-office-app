22711d784ff6e554a8e456a6edc42fa18358ed42
poojadewas <135793847+poojadewas@users.noreply.github.com>
2025-06-29 20:13:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,28 @@
-from flask import Flask, render_template, request, redirect
 import replicate
 import os
+from flask import Flask, render_template, request
+
+# Set your Replicate API token
+os.environ["REPLICATE_API_TOKEN"] = "r8_Af1yG4vRtHf7BVS47ZalRdSVZYttdQX0qdmdR"
 
 app = Flask(__name__)
 
-REPLICATE_API_TOKEN = os.environ.get("r8_8JNN22uZfVh9DqKbtOMnXlYInx1hs7l3WDsNU")
-
 @app.route("/", methods=["GET", "POST"])
 def index():
+    output_url = None
+
     if request.method == "POST":
+        # Save uploaded image
         image = request.files["image"]
         image.save("input.jpg")
 
-      output_url = replicate.run(
-    "tencentarc/gfpgan:0fbacf7afc6c144e5be9767cff80f25aff23e52b0708f17e20f9879b2f21516c",
-    input={"img": open("input.jpg", "rb")}
-)
+        # Run Replicate model
+        output_url = replicate.run(
+            "tencentarc/gfpgan:0fbacf7afc6c144e5be9767cff80f25aff23e52b0708f17e20f9879b2f21516c",
+            input={"img": open("input.jpg", "rb")}
+        )
 
-        return render_template("index.html", output_url=output_url)
-    return render_template("index.html", output_url=None)
+    return render_template("index.html", output_url=output_url)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    app.run(debug=True)
